1da4eb74a3bb8a7f525bbef8f9119bf566d0b749
minatokagaribi <minatokagaribi@gmail.com>
2021-10-10 19:18:52 +0900

    First Commit

---
+++ proj1/src/proj1/Sample1.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package proj1;
+
+/**
+ * @author nagom
+ *
+ */
+public class Sample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
