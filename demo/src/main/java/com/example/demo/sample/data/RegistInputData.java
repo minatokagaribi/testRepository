8811b55adea2ce21b8fa47631b58abadbf22bc74
minatokagaribi <minatokagaribi@gmail.com>
2021-10-24 15:19:50 +0900

    サーバサイドのコミット

---
+++ demo/src/main/java/com/example/demo/sample/data/RegistInputData.java
@@ -0,0 +1,61 @@
+/**
+ *
+ */
+package com.example.demo.sample.data;
+
+/**
+ * @author nagom
+ *
+ */
+public class RegistInputData {
+
+	private String id;
+
+	private String name;
+
+	private String email;
+
+	/**
+	 * @return id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id セットする id
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name セットする name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email セットする email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+}
